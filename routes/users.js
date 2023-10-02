bc5a54a57210f1d5a4ba0b671cdec0f95290cdaf
fernando-tecm <A01642285@tec.mx>
2023-10-02 17:17:39 -0600

    autenticacion con token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,27 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.send('login');
 });
+
+const users = [ //con base de datos
+  {username:'fernandogar', password:'holahola'},
+]
+
+router.post('/prueba', (req, res) => {
+  const username = req.body.username
+  const password = req.body.username
+
+  const authUser = users.find(user => user.username == username && user.password == password) //con base de datos
+  if(authUser) {
+    const token =jwt.sign({username:username},"SECRET")
+    if(token) {
+      res.json({token: token})
+    } else {
+      res.json({message: "Authentication failed", success: false})
+    }
+  } else {
+    res.json({message: "Authentication failed", success:false})
+  }
+
+})
 
 module.exports = router;
