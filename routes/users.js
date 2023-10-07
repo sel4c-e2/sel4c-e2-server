c9650c70531dba45fa386e01cb40188a0439fe10
Federico Zenteno <a01660986@tec.mx>
2023-10-07 16:09:20 -0600

    Post register

--- routes/users.js
+++ routes/users.js
@@ -52,6 +52,38 @@ router.get('/', function(req, res, next) {
 });
 
 // Register user
+// Register user
+router.post('/', async function(req, res, next) {
+  console.log(req.body)
+  const { name, lastname, email, password, gender, country_id } = req.body;
+  // Input validation
+  if (!name || !lastname || !email || !password || !gender || !country_id) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
+  console.log("--POST: /users--");
+
+  // Password hashing
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword)
+    const query = 'INSERT INTO users (name, email, password, gender, country_id) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [name, email, hashedPassword, gender, country_id];
+
+    connection.query(query, values, function (error, results, fields) {
+      if (error) {
+        console.error('Error creating new user:', error.stack);
+        return res.status(500).json({ message: 'Internal Server Error' });
+      }
+      console.log('User created successfully');
+      return res.status(201).json({ message: 'User created successfully' });
+    });
+    
+  } catch (hashError) {
+    console.error('Error hashing password:', hashError.stack);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 // Get a specific user by user_id
 router.get('/:id', function(req, res, next) {
   const userId = req.params.id;
