84f34ec4df177903515f78b9ae5cdcdb95379b0e
Esteban Zepeda <109043513+estzep@users.noreply.github.com>
2023-10-09 14:20:50 -0600

    Update db.js

--- db.js
+++ db.js
@@ -23,7 +23,7 @@
 
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
