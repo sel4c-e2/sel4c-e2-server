168313aefb3a125f1c3ac21d4dc0b85c95319742
Esteban Zepeda <109043513+estzep@users.noreply.github.com>
2023-10-09 16:08:11 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-try {
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to database:', err);
-            return;
-        }
-        console.log('Connected to database');
-    });
-} catch (error) {
-    console.log(error);
-}
+// try {
+//     connection.connect((err) => {
+//         if (err) {
+//             console.error('Error connecting to database:', err);
+//             return;
+//         }
+//         console.log('Connected to database');
+//     });
+// } catch (error) {
+//     console.log(error);
+// }
 
 module.exports = { connection };
 
